da195e463cf68679878ca0c7f6740bdabab67b4d
Chris Paredes <nealparedes776@gmail.com>
2026-05-06 19:45:12 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/About', (req, res) => {
+    res.render('about.ejs');
+});
+
+app.get('/Services', (req, res) => {
+    res.render('services.ejs');
+});
+
+app.get('/Contact', (req, res) => {
+    res.render('contact.ejs');
+});
+
+app.get("/Login", (req, res) => {
+    res.render('login.ejs');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
