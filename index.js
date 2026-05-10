4ff7c0a4d309b5567e8d1affbf3a728021b8a39e
Chris Paredes <nealparedes776@gmail.com>
2026-05-10 13:18:48 +0800

    Added and displayed books

--- index.js
+++ index.js
@@ -7,8 +7,159 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const books = [
+    {
+        "title": "Atomic Habits",
+        "author": "James Clear",
+        "category": ["Self Help"],
+        "image": "https://m.media-amazon.com/images/I/81wgcld4wxL._SL1500_.jpg"
+    },
+
+    {
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J.K. Rowling",
+        "category": ["Fantasy"],
+        "image": "https://m.media-amazon.com/images/I/81YOuOGFCJL._SL1500_.jpg"
+    },
+
+    {
+        "title": "Harry Potter and the Prisoner of Azkaban",
+        "author": "J.K. Rowling",
+        "category": ["Fantasy"],
+        "image": "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_65.zip&file=0014656700-L.jpg"
+    },
+
+    {
+        "title": "Harry Potter and the Goblet of Fire",
+        "author": "J.K. Rowling",
+        "category": ["Fantasy"],
+        "image": "https://m.media-amazon.com/images/I/81t2CVWEsUL._SL1500_.jpg"
+    },
+
+    {
+        "title": "Harry Potter and the Deathly Hallows",
+        "author": "J.K. Rowling",
+        "category": ["Fantasy"],
+        "image": "https://covers.openlibrary.org/b/id/7904782-L.jpg"
+    },
+
+    {
+        "title": "Harry Potter and the Half-Blood Prince",
+        "author": "J.K. Rowling",
+        "category": ["Fantasy"],
+        "image": "https://m.media-amazon.com/images/I/91OINeHnJGL._SL1500_.jpg"
+    },
+
+    {
+        "title": "Dune",
+        "author": "Frank Herbert",
+        "category": ["Science Fiction"],
+        "image": "https://covers.openlibrary.org/b/id/15092781-L.jpg"
+    },
+
+    {
+        "title": "Dune Messiah",
+        "author": "Frank Herbert",
+        "category": ["Science Fiction"],
+        "image": "https://covers.openlibrary.org/b/id/15209757-L.jpg"
+    },
+
+    {
+        "title": "Children of Dune",
+        "author": "Frank Herbert",
+        "category": ["Science Fiction"],
+        "image": "https://ia802908.us.archive.org/view_archive.php?archive=/24/items/olcovers28/olcovers28-L.zip&file=284479-L.jpg"
+    },
+
+    {
+        "title": "God Emperor of Dune",
+        "author": "Frank Herbert",
+        "category": ["Science Fiction"],
+        "image": "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_61.zip&file=0014612739-L.jpg"
+    },
+
+    {
+        "title": "Think and Grow Rich",
+        "author": "Napoleon Hill",
+        "category": ["Business"],
+        "image": "https://m.media-amazon.com/images/I/71UypkUjStL._SL1500_.jpg"
+    },
+
+    {
+        "title": "Lord of the Rings: The Fellowship of the Ring",
+        "author": "J.R.R. Tolkien",
+        "category": ["Fantasy"],
+        "image": "https://www.booksbykilo.in/media/books/BBK/9780261102354.jpg"
+    },
+
+    {
+        "title": "Lord of the Rings: The Two Towers",
+        "author": "J.R.R. Tolkien",
+        "category": ["Fantasy"],
+        "image": "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/The_20Two_20Towers_20_the_20Lord_20of_20the_20Rings_20Book_202.jpg?v=1716573397&width=400&crop=center"
+    },
+
+    {
+        "title": "Lord of the Rings: The Return of the King",
+        "author": "J.R.R. Tolkien",
+        "category": ["Fantasy"],
+        "image": "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/978-0-261-10237-8.jpg?v=1754439383&width=400&crop=center"
+    },
+
+    {
+        "title": "Goosebumps: Welcome to Dead House",
+        "author": "R.L. Stine",
+        "category": ["Horror", "Thriller"],
+        "image": "https://covers.openlibrary.org/b/id/14855397-L.jpg"
+    },
+
+    {
+        "title": "$100M Offers: How to Make Offers So Good People Feel Stupid Saying No",
+        "author": "Alex Hormozi",
+        "category": ["Business"],
+        "image": "https://ia801909.us.archive.org/view_archive.php?archive=/31/items/l_covers_0013/l_covers_0013_17.zip&file=0013173640-L.jpg"
+    },
+
+    {
+        "title": "The Fault in Our Stars",
+        "author": "John Green",
+        "category": ["Romance"],
+        "image": "https://m.media-amazon.com/images/I/817tHNcyAgL._SL1500_.jpg"
+    },
+
+    {
+        "title": "The 48 Laws of Power",
+        "author": "Robert Greene",
+        "category": ["Self Help"],
+        "image": "https://www.nateliason.com/_next/image?url=%2Fassets%2Fnotes%2F48-laws-power-robert-greene%2F48-laws-power-robert-greene-cover.jpg&w=640&q=75"
+    },
+
+    {
+        "title": "The 33 Strategies of War",
+        "author": "Robert Greene",
+        "category": ["Self Help"],
+        "image": "https://covers.openlibrary.org/b/isbn/9780670034574-L.jpg"
+    },
+
+    {
+        "title": "The Art of Seduction",
+        "author": "Robert Greene",
+        "category": ["Self Help"],
+        "image": "https://covers.openlibrary.org/b/isbn/9780142001196-L.jpg"
+    },
+
+    {
+        "title": "Rich Dad Poor Dad",
+        "author": "Robert Kiyosaki",
+        "category": ["Business", "Self Help"],
+        "image": "https://m.media-amazon.com/images/I/81bsw6fnUiL._SL1500_.jpg"
+    }
+]
+
+
 app.get('/', (req, res) => {
-    res.render('index.ejs', {loggedIn: false});
+    res.render('index.ejs', { loggedIn: false });
 });
 
 app.get('/About', (req, res) => {
@@ -18,32 +169,28 @@ app.get('/About', (req, res) => {
 });
 
 app.get('/Services', (req, res) => {
-    res.render('services.ejs', {loggedIn: false});
+    res.render('services.ejs', { loggedIn: false });
 });
 
 app.get('/Contact', (req, res) => {
-    res.render('contact.ejs', { loggedIn: false});
+    res.render('contact.ejs', { loggedIn: false });
 });
-
 
 app.get("/Login", (req, res) => {
     res.render('login.ejs');
 });
 
-
 app.post("/Dashboard", (req, res) => {
     const email = req.body['eMail'];
-    // const pass = req.body['passWord'];
 
     const studentReg = /^[a-z0-9._%+-]+@students\.nu-cebu\.edu\.ph$/i;
     const reg = /^[a-z0-9._%+-]+@nu-cebu\.edu\.ph$/i;
 
-    if (!studentReg.test(email.toString()) && !reg.test(email.toString())) {
+    if (!studentReg.test(email.toString()) &&
+        !reg.test(email.toString())) {
+
         return res.send("Invalid email address");
     }
-
-    // console.log("email: " + email);
-    // console.log("pass: " + pass);
 
     if (studentReg.test(email.toString())) {
         return res.redirect('/Students');
@@ -52,8 +199,26 @@ app.post("/Dashboard", (req, res) => {
     }
 });
 
+
 app.get('/Students', (req, res) => {
-    res.render("student.ejs", {loggedIn: true});
+
+    const booksPerPage = 9;
+    const page = parseInt(req.query.page) || 1;
+
+    const start = (page - 1) * booksPerPage;
+    const end = start + booksPerPage;
+
+    const paginatedBooks = books.slice(start, end);
+
+    const totalPages = Math.ceil(books.length / booksPerPage);
+
+    res.render("student.ejs", {
+        loggedIn: true,
+        books: paginatedBooks,
+        currentPage: page,
+        totalPages: totalPages
+    });
+
 });
 
 
