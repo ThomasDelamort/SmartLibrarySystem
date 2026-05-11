46596b1fe1596c2365c6d80e452acb9f083114ba
Chris Paredes <nealparedes776@gmail.com>
2026-05-11 21:07:00 +0800

    Few Updates

--- data/books.js
+++ data/books.js
@@ -1,150 +1,151 @@
 const books = [
     {
-        "title": "Atomic Habits",
-        "author": "James Clear",
-        "category": ["Self Help"],
-        "image": "https://m.media-amazon.com/images/I/81wgcld4wxL._SL1500_.jpg"
+        id: 1,
+        title: "Atomic Habits",
+        author: "James Clear",
+        category: ["Self Help"],
+        image: "https://m.media-amazon.com/images/I/81wgcld4wxL._SL1500_.jpg"
     },
-
     {
-        "title": "Harry Potter and the Philosopher's Stone",
-        "author": "J.K. Rowling",
-        "category": ["Fantasy"],
-        "image": "https://m.media-amazon.com/images/I/81YOuOGFCJL._SL1500_.jpg"
+        id: 2,
+        title: "Harry Potter and the Philosopher's Stone",
+        author: "J.K. Rowling",
+        category: ["Fantasy"],
+        image: "https://m.media-amazon.com/images/I/81YOuOGFCJL._SL1500_.jpg"
     },
-
     {
-        "title": "Harry Potter and the Prisoner of Azkaban",
-        "author": "J.K. Rowling",
-        "category": ["Fantasy"],
-        "image": "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_65.zip&file=0014656700-L.jpg"
+        id: 3,
+        title: "Harry Potter and the Prisoner of Azkaban",
+        author: "J.K. Rowling",
+        category: ["Fantasy"],
+        image: "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_65.zip&file=0014656700-L.jpg"
     },
-
     {
-        "title": "Harry Potter and the Goblet of Fire",
-        "author": "J.K. Rowling",
-        "category": ["Fantasy"],
-        "image": "https://m.media-amazon.com/images/I/81t2CVWEsUL._SL1500_.jpg"
+        id: 4,
+        title: "Harry Potter and the Goblet of Fire",
+        author: "J.K. Rowling",
+        category: ["Fantasy"],
+        image: "https://m.media-amazon.com/images/I/81t2CVWEsUL._SL1500_.jpg"
     },
-
     {
-        "title": "Harry Potter and the Deathly Hallows",
-        "author": "J.K. Rowling",
-        "category": ["Fantasy"],
-        "image": "https://covers.openlibrary.org/b/id/7904782-L.jpg"
+        id: 5,
+        title: "Harry Potter and the Deathly Hallows",
+        author: "J.K. Rowling",
+        category: ["Fantasy"],
+        image: "https://covers.openlibrary.org/b/id/7904782-L.jpg"
     },
-
     {
-        "title": "Harry Potter and the Half-Blood Prince",
-        "author": "J.K. Rowling",
-        "category": ["Fantasy"],
-        "image": "https://m.media-amazon.com/images/I/91OINeHnJGL._SL1500_.jpg"
+        id: 6,
+        title: "Harry Potter and the Half-Blood Prince",
+        author: "J.K. Rowling",
+        category: ["Fantasy"],
+        image: "https://m.media-amazon.com/images/I/91OINeHnJGL._SL1500_.jpg"
     },
-
     {
-        "title": "Dune",
-        "author": "Frank Herbert",
-        "category": ["Science Fiction"],
-        "image": "https://covers.openlibrary.org/b/id/15092781-L.jpg"
+        id: 7,
+        title: "Dune",
+        author: "Frank Herbert",
+        category: ["Science Fiction"],
+        image: "https://covers.openlibrary.org/b/id/15092781-L.jpg"
     },
-
     {
-        "title": "Dune Messiah",
-        "author": "Frank Herbert",
-        "category": ["Science Fiction"],
-        "image": "https://covers.openlibrary.org/b/id/15209757-L.jpg"
+        id: 8,
+        title: "Dune Messiah",
+        author: "Frank Herbert",
+        category: ["Science Fiction"],
+        image: "https://covers.openlibrary.org/b/id/15209757-L.jpg"
     },
-
     {
-        "title": "Children of Dune",
-        "author": "Frank Herbert",
-        "category": ["Science Fiction"],
-        "image": "https://ia802908.us.archive.org/view_archive.php?archive=/24/items/olcovers28/olcovers28-L.zip&file=284479-L.jpg"
+        id: 9,
+        title: "Children of Dune",
+        author: "Frank Herbert",
+        category: ["Science Fiction"],
+        image: "https://ia802908.us.archive.org/view_archive.php?archive=/24/items/olcovers28/olcovers28-L.zip&file=284479-L.jpg"
     },
-
     {
-        "title": "God Emperor of Dune",
-        "author": "Frank Herbert",
-        "category": ["Science Fiction"],
-        "image": "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_61.zip&file=0014612739-L.jpg"
+        id: 10,
+        title: "God Emperor of Dune",
+        author: "Frank Herbert",
+        category: ["Science Fiction"],
+        image: "https://ia600505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_61.zip&file=0014612739-L.jpg"
     },
-
     {
-        "title": "Think and Grow Rich",
-        "author": "Napoleon Hill",
-        "category": ["Business"],
-        "image": "https://m.media-amazon.com/images/I/71UypkUjStL._SL1500_.jpg"
+        id: 11,
+        title: "Think and Grow Rich",
+        author: "Napoleon Hill",
+        category: ["Business"],
+        image: "https://m.media-amazon.com/images/I/71UypkUjStL._SL1500_.jpg"
     },
-
     {
-        "title": "Lord of the Rings: The Fellowship of the Ring",
-        "author": "J.R.R. Tolkien",
-        "category": ["Fantasy"],
-        "image": "https://www.booksbykilo.in/media/books/BBK/9780261102354.jpg"
+        id: 12,
+        title: "Lord of the Rings: The Fellowship of the Ring",
+        author: "J.R.R. Tolkien",
+        category: ["Fantasy"],
+        image: "https://www.booksbykilo.in/media/books/BBK/9780261102354.jpg"
     },
-
     {
-        "title": "Lord of the Rings: The Two Towers",
-        "author": "J.R.R. Tolkien",
-        "category": ["Fantasy"],
-        "image": "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/The_20Two_20Towers_20_the_20Lord_20of_20the_20Rings_20Book_202.jpg?v=1716573397&width=400&crop=center"
+        id: 13,
+        title: "Lord of the Rings: The Two Towers",
+        author: "J.R.R. Tolkien",
+        category: ["Fantasy"],
+        image: "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/The_20Two_20Towers_20_the_20Lord_20of_20the_20Rings_20Book_202.jpg?v=1716573397&width=400&crop=center"
     },
-
     {
-        "title": "Lord of the Rings: The Return of the King",
-        "author": "J.R.R. Tolkien",
-        "category": ["Fantasy"],
-        "image": "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/978-0-261-10237-8.jpg?v=1754439383&width=400&crop=center"
+        id: 14,
+        title: "Lord of the Rings: The Return of the King",
+        author: "J.R.R. Tolkien",
+        category: ["Fantasy"],
+        image: "https://cdn.shopify.com/s/files/1/0625/6679/3413/files/978-0-261-10237-8.jpg?v=1754439383&width=400&crop=center"
     },
-
     {
-        "title": "Goosebumps: Welcome to Dead House",
-        "author": "R.L. Stine",
-        "category": ["Horror", "Thriller"],
-        "image": "https://covers.openlibrary.org/b/id/14855397-L.jpg"
+        id: 15,
+        title: "Goosebumps: Welcome to Dead House",
+        author: "R.L. Stine",
+        category: ["Horror", "Thriller"],
+        image: "https://covers.openlibrary.org/b/id/14855397-L.jpg"
     },
-
     {
-        "title": "$100M Offers: How to Make Offers So Good People Feel Stupid Saying No",
-        "author": "Alex Hormozi",
-        "category": ["Business"],
-        "image": "https://ia801909.us.archive.org/view_archive.php?archive=/31/items/l_covers_0013/l_covers_0013_17.zip&file=0013173640-L.jpg"
+        id: 16,
+        title: "$100M Offers: How to Make Offers So Good People Feel Stupid Saying No",
+        author: "Alex Hormozi",
+        category: ["Business"],
+        image: "https://ia801909.us.archive.org/view_archive.php?archive=/31/items/l_covers_0013/l_covers_0013_17.zip&file=0013173640-L.jpg"
     },
-
     {
-        "title": "The Fault in Our Stars",
-        "author": "John Green",
-        "category": ["Romance"],
-        "image": "https://m.media-amazon.com/images/I/817tHNcyAgL._SL1500_.jpg"
+        id: 17,
+        title: "The Fault in Our Stars",
+        author: "John Green",
+        category: ["Romance"],
+        image: "https://m.media-amazon.com/images/I/817tHNcyAgL._SL1500_.jpg"
     },
-
     {
-        "title": "The 48 Laws of Power",
-        "author": "Robert Greene",
-        "category": ["Self Help"],
-        "image": "https://covers.openlibrary.org/b/id/15177890-L.jpg"
+        id: 18,
+        title: "The 48 Laws of Power",
+        author: "Robert Greene",
+        category: ["Self Help"],
+        image: "https://covers.openlibrary.org/b/id/15177890-L.jpg"
     },
-
     {
-        "title": "The 33 Strategies of War",
-        "author": "Robert Greene",
-        "category": ["Self Help"],
-        "image": "https://covers.openlibrary.org/b/isbn/9780670034574-L.jpg"
+        id: 19,
+        title: "The 33 Strategies of War",
+        author: "Robert Greene",
+        category: ["Self Help"],
+        image: "https://covers.openlibrary.org/b/isbn/9780670034574-L.jpg"
     },
-
     {
-        "title": "The Art of Seduction",
-        "author": "Robert Greene",
-        "category": ["Self Help"],
-        "image": "https://covers.openlibrary.org/b/isbn/9780142001196-L.jpg"
+        id: 20,
+        title: "The Art of Seduction",
+        author: "Robert Greene",
+        category: ["Self Help"],
+        image: "https://covers.openlibrary.org/b/isbn/9780142001196-L.jpg"
     },
-
     {
-        "title": "Rich Dad Poor Dad",
-        "author": "Robert Kiyosaki",
-        "category": ["Business", "Self Help"],
-        "image": "https://m.media-amazon.com/images/I/81bsw6fnUiL._SL1500_.jpg"
+        id: 21,
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        category: ["Business", "Self Help"],
+        image: "https://m.media-amazon.com/images/I/81bsw6fnUiL._SL1500_.jpg"
     }
-]
+];
 
 export default books;
--- routes/students.js
+++ routes/students.js
@@ -12,7 +12,6 @@ router.get('/Students', (req, res) => {
     const end = start + booksPerPage;
 
     const paginatedBooks = books.slice(start, end);
-
     const totalPages = Math.ceil(books.length / booksPerPage);
 
     res.render('student.ejs', {
@@ -24,17 +23,40 @@ router.get('/Students', (req, res) => {
 });
 
 
-// router.post('/Submit', (req, res) => {
-//
-// });
+router.post('/Submit', (req, res) => {
+    console.log("BODY RECEIVED:", req.body);
 
-router.get('/Students/Book', (req, res) => {
-   const book = books[0];
+    const { id, action } = req.body;
 
-   res.render("book.ejs", {
-      loggedIn: true,
-      book
-   });
+    const book = books.find(b => b.id === Number(id));
+
+    if (!book) {
+        console.log("BOOK NOT FOUND FOR ID:", id);
+        return res.status(404).send("Book not found");
+    }
+
+    console.log("FOUND BOOK:", book);
+
+    if (action === "borrow") {
+        return res.redirect(`/Students/Book/${book.id}`);
+    }
+});
+
+
+router.get('/Students/Book/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    const book = books.find(b => b.id === Number(id));
+
+    if (!book) {
+        return res.status(404).send("Book not found");
+    }
+
+    res.render("book.ejs", {
+        loggedIn: true,
+        book
+    });
 });
 
 export default router;
