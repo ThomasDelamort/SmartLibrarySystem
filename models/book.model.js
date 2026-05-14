15adf9564111661a3137704fcb49d7837266e4c1
Chris Paredes <nealparedes776@gmail.com>
2026-05-14 10:18:54 +0800

    Added Search Function

---
+++ models/book.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    category: {
+        type: [String],
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
