9307118e6efea43717b087d110ab4d409c21659b
Chris Paredes <nealparedes776@gmail.com>
2026-05-14 19:17:36 +0800

    Added Search Function

--- models/student.model.js
+++ models/student.model.js
@@ -1,18 +1,70 @@
 import mongoose from "mongoose";
 
-const studentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-});
+const studentSchema = new mongoose.Schema(
+    {
+        studentId: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
 
-export default mongoose.model('Student', studentSchema);
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        borrowedBooks: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book",
+            }
+        ],
+
+        reservations: [
+            {
+            room: String,
+            timeSlot: String,
+            data: Date
+            }
+        ],
+
+        fines: {
+            type: Number,
+            default: 0,
+            min: 0
+        },
+
+        role: {
+            type: String,
+            default: 'student'
+        }
+    },
+{
+        timestamps: true
+    }
+);
+
+const student = mongoose.model('Student', studentSchema);
+
+export default student;
