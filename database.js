1065ad6ace718097e0faf467a8add95f0f7d9965
Chris Paredes <nealparedes776@gmail.com>
2026-05-23 14:20:24 +0800

    Initial Commit for Authentication Overhaul

--- database.js
+++ database.js
@@ -1,21 +1,16 @@
-import mongoose from 'mongoose';
-import {app} from './index.js'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
-dotenv.config()
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected");
+    } catch (error) {
 
-const port = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI;
+        console.log("Database Connection Error");
+        console.log(error);
 
-async function devann(){
-    try{
-        await mongoose.connect(mongoURI);
-        console.log("Connected to DB");
-        app.listen(port, () => console.log(`Server started on port ${port}`));
-    }catch(err){
-        console.log(err);
         process.exit(1);
     }
-}
+};
 
-export default devann;
+export default connectDB;
