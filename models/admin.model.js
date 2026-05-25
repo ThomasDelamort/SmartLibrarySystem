7a11aed80f3107cc785650ebadfaf34b525f6026
Chris Paredes <nealparedes776@gmail.com>
2026-05-25 12:20:08 +0800

    Updated Admin Model

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,41 +1,42 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema({
-    adminId:   {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
+const adminSchema = new mongoose.Schema(
+    {
+        adminId:   {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
 
-    firstName: {
-        type: String,
-        required: true,
-    },
+        firstName: {
+            type: String,
+            required: true,
+        },
 
-    lastName: {
-        type: String,
-        required: true,
-    },
+        lastName: {
+            type: String,
+            required: true,
+        },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
 
-    password: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+        password: {
+            type: String,
+            required: true,
+            unique: true,
+        },
 
-    role: {
-        type: String,
-        required: true,
-        default: 'admin',
+        role: {
+            type: String,
+            required: true,
+            default: 'admin',
+        },
     },
-},
     {
         timestamps: true
     }
