7a9873cf75187fab4587eabe0233343ff656da73
Chris Paredes <nealparedes776@gmail.com>
2026-06-12 19:22:18 +0800

    React init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import MainPage from './pages/MainPage/MainPage'
+import './App.css'
+
+function App() {
+   return (
+    <>
+      <MainPage />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Heading from './components/Header'
+
+const MainPage = () => {
+    return (
+        <Heading />
+    )
+}
+export default MainPage
